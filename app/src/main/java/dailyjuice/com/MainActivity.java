06f803a9327efaa84440254a7a7a29ff42bfb62e
llicar3 <60633372+llicar3@users.noreply.github.com>
2020-04-27 20:23:23 -0500

    finished buttons on first page

--- app/src/main/java/dailyjuice/com/MainActivity.java
+++ app/src/main/java/dailyjuice/com/MainActivity.java
@@ -9,6 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -65,6 +74,37 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public class QuoteOfTheDay {
+
+        public static void main(String[] args) throws IOException {
+            URL url = new URL("https://quotes.rest/qod?category=funny");
+
+            try{
+                //make connection
+                HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+                urlc.setRequestMethod("GET");
+                // set the content type
+                urlc.setRequestProperty("Content-Type", "application/json");
+                urlc.setRequestProperty("X-TheySaidSo-Api-Secret", "YOUR API KEY HERE");
+                System.out.println("Connect to: " + url.toString());
+                urlc.setAllowUserInteraction(false);
+                urlc.connect();
+
+                //get result
+                BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+                String l = null;
+                while ((l=br.readLine())!=null) {
+                    System.out.println(l);
+                }
+                br.close();
+            } catch (Exception e){
+                System.out.println("Error occured");
+                System.out.println(e.toString());
+            }
+        }
+
     }
 }
 
--- app/src/main/java/dailyjuice/com/SecondActivity.java
+++ app/src/main/java/dailyjuice/com/SecondActivity.java
@@ -2,7 +2,12 @@ package dailyjuice.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button backSpace = (Button) findViewById(R.id.QOD);
+        backSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+                Log.d("Daily Juice", "my app");
+                Toast.makeText(getApplicationContext(),"Juicing!", Toast.LENGTH_SHORT).show();
+                moveToActivityTwo();
     }
 }
