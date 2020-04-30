87574b966b500681e79ca88724a24fd91b988cfa
llicar3 <60633372+llicar3@users.noreply.github.com>
2020-04-29 19:40:15 -0500

    finished buttons on first page

--- app/src/main/java/dailyjuice/com/MainActivity.java
+++ app/src/main/java/dailyjuice/com/MainActivity.java
@@ -1,8 +1,11 @@
 package dailyjuice.com;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,6 +15,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.HttpURLConnection;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +24,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public class buttonColor() {
-
-    }
+        Button QOD = (Button) findViewById(R.id.QOD);
+        ------intent.getextra("QOD", String);
+        QOD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+                Log.d("Daily Juice", "my app");
+                Toast.makeText(getApplicationContext(),"Juicing!", Toast.LENGTH_SHORT).show();
+                requestQuote("quoteoftheday");
+                moveToActivityTwo();
+            }
+        });
+        final Button funnyButton = (Button) findViewById(R.id.funnyButton);
+        funnyButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+                Log.d("Daily Juice", "my app");
+                Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
+                requestQuote("funny");
+                moveToActivityTwo();
+            }
+        }));
+        Button loveButton = (Button) findViewById(R.id.loveButton);
+        loveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+                Log.d("Daily Juice", "my app");
+                Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
+                requestQuote("love");
+                moveToActivityTwo();
+            }
+        });
+        Button artButton = (Button) findViewById(R.id.artbutton);
+        artButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+                Log.d("Daily Juice", "my app");
+                Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
+                requestQuote("art");
+                moveToActivityTwo();
+            }
+        });
+        Button natureButton = (Button) findViewById(R.id.natureButton);
+        natureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+                Log.d("Daily Juice", "my app");
+                Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
+                requestQuote("nature");
+                moveToActivityTWo();
+            }
+        });
 
     public class QuoteOfTheDay {
 
         public static void main(String[] args) throws IOException {
-            URL url = new URL("https://quotes.rest/qod?category=funny");
+
+        }
+
+        public static requestQuote(String category) {
+            try {
+                String urlString = "https://quotes.rest/qod?category=" + category;
+                URL url = new URL(urlString);
+            } catch (Exception e) {
+                System.out.println("malformed url");
+            }
+
 
             try{
                 //make connection
@@ -42,6 +109,12 @@ public class MainActivity extends AppCompatActivity {
                 urlc.connect();
 
                 //get result
+                StringBuilder response = new StringBuilder(); 
+                String currentLine;
+                while ((currentLine = in.readLine()) != null) {
+                        response.append(currentLine); 
+                in.close();
+                return response.toString();
                 BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
                 String l = null;
                 while ((l=br.readLine())!=null) {
@@ -53,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(e.toString());
             }
         }
-
     }
-}
+
+
 
--- app/src/main/java/dailyjuice/com/SecondActivity.java
+++ app/src/main/java/dailyjuice/com/SecondActivity.java
@@ -2,6 +2,7 @@ package dailyjuice.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,14 +15,16 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
         setContentView(R.layout.activity_second);
-        Button backSpace = (Button) findViewById(R.id.QOD);
-        backSpace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.setBackgroundColor(Color.GREEN);
-                Log.d("Daily Juice", "my app");
-                Toast.makeText(getApplicationContext(),"Juicing!", Toast.LENGTH_SHORT).show();
-                moveToActivityTwo();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Button backSpace = (Button) findViewById(R.id.QOD);
+        //backSpace.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+                //v.setBackgroundColor(Color.GREEN);
+                //Log.d("Daily Juice", "my app");
+                //Toast.makeText(getApplicationContext(),"Juicing!", Toast.LENGTH_SHORT).show();
+
     }
 }
