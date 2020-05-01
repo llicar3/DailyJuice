d0c51db8f3b0a6228aa3385830894e3376155b48
llicar3 <60633372+llicar3@users.noreply.github.com>
2020-04-30 22:34:40 -0500

    finished buttons on first page

--- app/src/main/java/dailyjuice/com/MainActivity.java
+++ app/src/main/java/dailyjuice/com/MainActivity.java
@@ -22,118 +22,128 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button QOD = (Button) findViewById(R.id.QOD);
-        Intent qodIntent = new Intent(this, SecondActivity.class);
-        startActivity(qodIntent);
-        //intent.getextra("QOD", String);
         QOD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent qodIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 v.setBackgroundColor(Color.GREEN);
                 Log.d("Daily Juice", "my app");
-                Toast.makeText(getApplicationContext(),"Juicing!", Toast.LENGTH_SHORT).show();
-                requestQuote("quoteoftheday");
+                Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
+                Quote QOD = new Quote();
+                String quote = QOD.requestQuote("QOD");
+                qodIntent.putExtra("quote", quote);
+                startActivity(qodIntent);
                 //startActivity(qodIntent);
 
             }
         });
+
         Button funnyButton = (Button) findViewById(R.id.funnyButton);
-        Intent funnyIntent = new Intent(this, SecondActivity.class);
-        startActivity(funnyIntent);
         funnyButton.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent funnyIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 v.setBackgroundColor(Color.GREEN);
                 Log.d("Daily Juice", "my app");
                 Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
-                requestQuote("funny");
+                Quote q = new Quote();
+                String quote = q.requestQuote("funny");
+                funnyIntent.putExtra("quote", quote);
+                startActivity(funnyIntent);
             }
         }));
+
         Button loveButton = (Button) findViewById(R.id.loveButton);
-        Intent loveIntent = new Intent(this, SecondActivity.class);
-        startActivity(loveIntent);
         loveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent loveIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 v.setBackgroundColor(Color.GREEN);
                 Log.d("Daily Juice", "my app");
                 Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
-                requestQuote("love");
+                Quote q = new Quote();
+                String quote = q.requestQuote("love");
+                loveIntent.putExtra("quote", quote);
+                startActivity(loveIntent);
             }
         });
+
         Button artButton = (Button) findViewById(R.id.artbutton);
-        Intent artIntent = new Intent(this, SecondActivity.class);
-        startActivity(artIntent);
         artButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent artIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 v.setBackgroundColor(Color.GREEN);
                 Log.d("Daily Juice", "my app");
                 Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
-                requestQuote("art");
+                Quote q = new Quote();
+                String quote = q.requestQuote("art");
+                artIntent.putExtra("quote", quote);
+                startActivity(artIntent);
             }
         });
+
         Button natureButton = (Button) findViewById(R.id.natureButton);
-        Intent natureIntent = new Intent(this, SecondActivity.class);
-        startActivity(natureIntent);
         natureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent natureIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 v.setBackgroundColor(Color.GREEN);
                 Log.d("Daily Juice", "my app");
                 Toast.makeText(getApplicationContext(), "Juicing!", Toast.LENGTH_SHORT).show();
-                requestQuote("nature");
+                Quote q = new Quote();
+                String quote = q.requestQuote("nature");
+                natureIntent.putExtra("quote", quote);
+                startActivity(natureIntent);
             }
         });
+    }
+};
 
-    public class QuoteOfTheDay {
-
-        public static void main(String[] args) throws IOException {
-
+class Quote {
+    public String requestQuote(String category){
+        try {
+            String urlString = "https://quotes.rest/qod?category=" + category;
+            URL url = new URL(urlString);
+        } catch (Exception e) {
+            System.out.println("malformed url");
         }
 
-        public static requestQuote(String category) {
-            try {
-                String urlString = "https://quotes.rest/qod?category=" + category;
-                URL url = new URL(urlString);
-            } catch (Exception e) {
-                System.out.println("malformed url");
-            }
+        try {
+            //make connection
+            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+            urlc.setRequestMethod("GET");
+            // set the content type
+            urlc.setRequestProperty("Content-Type", "application/json");
+            urlc.setRequestProperty("X-TheySaidSo-Api-Secret", "YOUR API KEY HERE");
+            System.out.println("Connect to: " + url.toString());
+            urlc.setAllowUserInteraction(false);
+            urlc.connect();
 
-            try {
-                //make connection
-                HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
-                urlc.setRequestMethod("GET");
-                // set the content type
-                urlc.setRequestProperty("Content-Type", "application/json");
-                urlc.setRequestProperty("X-TheySaidSo-Api-Secret", "YOUR API KEY HERE");
-                System.out.println("Connect to: " + url.toString());
-                urlc.setAllowUserInteraction(false);
-                urlc.connect();
-
-                //get result
-                StringBuilder response = new StringBuilder(); 
-                String currentLine;
-                while ((currentLine = in.readLine()) != null) {
-                        response.append(currentLine); 
-                in.close();
-                return response.toString();
-                BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-                String l = null;
-                while ((l=br.readLine())!=null) {
-                    System.out.println(l);
-                }
-                br.close();
-            } catch (Exception e){
-                System.out.println("Error occured");
-                System.out.println(e.toString());
+            //get result
+            StringBuilder response = new StringBuilder(); 
+            String currentLine;
+            while ((currentLine = in.readLine()) != null) {
+         response.append(currentLine); 
             }
+            in.close();
+            return response.toString();
+//                BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+//              String l = null;
+//                while ((l = br.readLine()) != null) {
+//                    System.out.println(l);
+//               }
+//                br.close();
+        } catch (Exception e) {
+            System.out.println("Error occurred");
+            System.out.println(e.toString());
         }
     }
-
+};
 
 
